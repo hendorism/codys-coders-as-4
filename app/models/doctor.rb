8a56519ad745f6d1a5db43f9839910b70b59dfa0
Aaron Dull <hendorism@gmail.com>
2021-02-25 06:14:43 -0800

    commit from galaxy s7 in termux

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,3 +2,7 @@ class Doctor < ApplicationRecord
     has_many :appointments
     has_many :patients, through: :appointments
 end
+# this comment being typed
+# into a keyboard into
+# galaxy s7 running
+# vim inside termux!
