259f2472f63fffc5a14015eecf6e3cd563105579
hendorism <hendorism@gmail.com>
2021-02-25 04:44:20 -0800

    almost complete

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -64,6 +64,58 @@ class AppointmentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def appointment_params
-      params.require(:appointment).permit(:date, :time, :purpose, :notes)
+      params.require(:appointment).permit(:date, :time, :purpose, :notes, :patient_id, :doctor_id)
+      # from as-3
+      # params.require(:reply).permit(:body, :user_id, :post_id)
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# i am a jelly donut.
+# my friends call me sam
+# they want to eat me
+# jelly donut that i am
+# chase me up and down the hills they do
+# burned every calorie
+# now i am fit donut
+# but only to fit in your mouth
+# if your dietary concerns dont prohibit
+# the eating by you of me
+# i am a donut
+# not free
+# in the possession of a hungry hand
+# of crunching teeth
+# of the eating man or woman
+# who has come to consume
+# me the donut
+# fit but with jelly also remaining
+# jelly for one more day of explaining
+# i am a jelly donut
+# having come here
+# having come here so close to where the breath
+# causes me chills
+# and i am discarded half eaten
+# where coffee spills
+# my jelly and powdered sugar
+# are lying not in or on me
+# but around and down about me
+# but i am a caloric device
+# a fuel
+# flaming entropy enters an eddy
+# where a conscious entity
+# makes a software desicion
